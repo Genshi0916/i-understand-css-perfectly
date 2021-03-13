6aeec0c5249a2598cbd69e1494c5ea37167f174b
Genshi0916 <genshi0916@gmail.com>
2021-03-13 22:19:38 +0900

    :sparkles: ファイルの整理とheader,footerの追加

---
+++ src/components/Footer.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    appBar: {
+      top: 'auto',
+      bottom: 0,
+    },
+    grow: {
+      flexGrow: 1,
+    },
+    text: {
+      padding: theme.spacing(0.5),
+    }
+  }),
+);
+
+export default function BottomAppBar() {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <AppBar position="fixed" style={{ backgroundColor: "#44bc84" }} className={classes.appBar}>
+        <Toolbar>
+          <div className={classes.grow} />
+          <MailOutlineIcon />
+          <div className={classes.text}>genshi0916@gmail.com</div>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
+}
